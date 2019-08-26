3b6cdd8d33cec240600e72a2614d57b0b0650691
Gaivnwhy <guowenwhy@gmail.com>
2019-08-26 19:45:32 +0800

    Add files via upload

--- CImain.py
+++ CImain.py
@@ -6,3 +6,14 @@ class CImain:
 	def __init__(self):
 		# 定义相应目录
 		config = ConfigParser()
+	
+	def get_resource(self):
+	# update获取更新
+	
+	def build_resource(self):
+	# 构建war包
+	
+	def deploy_resource(self):
+	# 复制替换
+	
+	def check_version(self):
